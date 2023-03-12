4a458b3b5f4dc3c7a15a1e9ddbb10901643d6638
Cody Perry <perrysdevelopment@gmail.com>
2023-03-12 19:54:47 -0400

    Upload firebase config so far

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    signInWithRedirect, 
+    signInWithPopup, 
+    GoogleAuthProvider} 
+from "firebase/auth";
+import { 
+    getFirestore, 
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
+
+// Create an app instance based on config
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCmzvYTl2gy-bYmOjY2Lru8cXxf2Bq0Kos",
+    authDomain: "e-shop-db-98d54.firebaseapp.com",
+    projectId: "e-shop-db-98d54",
+    storageBucket: "e-shop-db-98d54.appspot.com",
+    messagingSenderId: "61867970605",
+    appId: "1:61867970605:web:e0a0f25ee967eaa44f28a4"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();
+
+  provider.setCustomParameters({
+    prompt: "select_account"
+  });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+  }
